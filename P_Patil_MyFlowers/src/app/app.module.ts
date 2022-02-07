cd390036f240946140431b9e686acedad3e16841
Prarthana Purav Patil <W0766002@myscc.ca>
2022-02-06 19:55:59 -0500

    complete Assignment 1

--- P_Patil_MyFlowers/src/app/app.module.ts
+++ P_Patil_MyFlowers/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { YComponent } from './y/y.component';
+
 import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    YComponent,
+  
     ContentCardComponent
   ],
   imports: [
